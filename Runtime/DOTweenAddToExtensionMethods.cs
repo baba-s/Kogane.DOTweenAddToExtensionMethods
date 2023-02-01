29acd032dc3c6188adb96e74e753d7b579314618
baba-s <shotababa1206@gmail.com>
2023-02-01 21:01:03 +0900

    add

--- Runtime/DOTweenAddToExtensionMethods.cs
+++ Runtime/DOTweenAddToExtensionMethods.cs
@@ -1,4 +1,6 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
 
@@ -18,18 +20,37 @@ namespace Kogane
         // 関数(static)
         //================================================================================
         /// <summary>
+        /// 指定された CancellationToken がキャンセルされた時に Tween をキャンセルします
+        /// </summary>
+        public static async UniTask AddTo
+        (
+            this Tween            self,
+            CancellationToken     cancellationToken,
+            TweenCancelBehaviour? tweenCancelBehaviour = null
+        )
+        {
+            await self.ToUniTask
+            (
+                tweenCancelBehaviour: tweenCancelBehaviour ?? DefaultTweenCancelBehaviour,
+                cancellationToken: cancellationToken
+            );
+        }
+
+        /// <summary>
         /// 指定されたゲームオブジェクトが破棄された時に Tween をキャンセルします
         /// </summary>
-        public static UniTask AddTo
+        public static async UniTask AddTo
         (
             this Tween            self,
             GameObject            gameObject,
             TweenCancelBehaviour? tweenCancelBehaviour = null
         )
         {
+            if ( gameObject == null ) throw new OperationCanceledException();
+
             var cancellationToken = gameObject.GetCancellationTokenOnDestroy();
 
-            return self
+            await self
                     .SetLink( gameObject )
                     .ToUniTask( tweenCancelBehaviour ?? DefaultTweenCancelBehaviour, cancellationToken )
                 ;
@@ -38,14 +59,16 @@ namespace Kogane
         /// <summary>
         /// 指定されたゲームオブジェクトが破棄された時に Tween をキャンセルします
         /// </summary>
-        public static UniTask AddTo
+        public static async UniTask AddTo
         (
             this Tween            self,
             Component             component,
             TweenCancelBehaviour? tweenCancelBehaviour = null
         )
         {
-            return self.AddTo( component.gameObject, tweenCancelBehaviour );
+            if ( component == null ) throw new OperationCanceledException();
+
+            await self.AddTo( component.gameObject, tweenCancelBehaviour );
         }
     }
 }
